f631b15df07534b1f2b379360f5c95b0c61cd43d
Cybandy <andyboat75@gmail.com>
2024-08-15 22:52:13 +0200

    passed some testing

--- main.py
+++ main.py
@@ -1,40 +1,32 @@
-import time
-import json
-
-from selenium import webdriver
-
-from selenium.webdriver.common.by import By
-
-from selenium.webdriver.common.keys import Keys
-
-from selenium.webdriver.common.action_chains import ActionChains
-
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-
+from utilities.driver import WebDriver
 from pprint import pprint
 
-import concurrent.futures
 
-options = Options()
-options.add_argument("start-maximized")
-options.add_argument('--headless=new')
-options.add_argument('--disable-dev-shm-usage')
-options.add_argument('--disable-gpu')
-options.add_argument('--no-sandbox')
-driver = webdriver.Chrome(options=options)
+class Scrapper(WebDriver):
+    # always accept main pages url
+    def __init__(self, url, type):
+        super().__init__(url, type)
 
+    def start(self):
+        # open chrome driver
+        self.open()
+        # readLinks
+        self.readLinks()
+        print(f'Number of links: {len(self.links)}')
+        if (len(self.links) > 0):
+            for link in self.links[:1]:
+                self.readPage(link)
 
-def main():
-    driver.get("https://www.hymnalaccompanist.com/twi/twinumber.html")
-    links = driver.find_elements(by=By.CSS_SELECTOR, value='a.hintanchor')
-    links = [link.get_attribute('href') for link in links]
-    pprint(links)
-    driver.quit()
+        self.writeJSON(self.json_data)
+        self.reset()
+        self.stop()
 
 
 if (__name__ == '__main__'):
     m_url = "https://www.hymnalaccompanist.com/twi/twinumber.html"
     n_url = "https://www.hymnalaccompanist.com/twi/n-title.html"
     ng_url = "https://www.hymnalaccompanist.com/twi/ng-title.html"
-    main()
+    scrap = Scrapper(m_url, 'num')
+    # scrap = Scrapper(n_url, 'n')
+    # scrap = Scrapper(ng, 'ng')
+    scrap.start()
--- utilities/driver.py
+++ utilities/driver.py
@@ -4,30 +4,97 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+import os
+import json
 
 
 class WebDriver:
+    options = Options()
+    url = ''
+    type = ''  # num, n, ng
+    driver = None
+    links = []
+    json_data = []
+    twi_translation_table = str.maketrans('xXqQ', 'ɔƆɛƐ')
 
-    def __init__(self, url):
+    def __init__(self, url, type):
         assert isinstance(url, str)
+        assert isinstance(type, str)
         self.url = url
-        self.links = []
-        self.options = Options()
+        self.type = type
         self.options.add_argument("start-maximized")
         self.options.add_argument('--headless=new')
         self.options.add_argument('--disable-dev-shm-usage')
-        self.options.add_argument('--disable-gpu')
+        # self.options.add_argument('--disable-gpu')
         self.options.add_argument('--no-sandbox')
-        self.driver = webdriver.chrome(self.options)
-        self.driver.get(url)
+
+    def open(self):
+        self.driver = webdriver.Chrome(options=self.options)
+        self.driver.get(self.url)
 
     def stop(self):
         self.driver.quit()
 
     def readLinks(self):
-        links = self.driver.find_elements(
-            by=By.CSS_SELECTOR, value='a.hintanchor')
-        self.links = [link.get_attribute('href') for link in links]
+        if (self.type == 'ng'):
+            tr_link = self.driver.find_elements(by=By.TAG_NAME, value='td')
+            links = tr_link[-1].find_elements(
+                by=By.CSS_SELECTOR, value='a')
+            self.links = [link.get_attribute('href') for link in links]
+        else:
+            links = self.driver.find_elements(
+                by=By.CSS_SELECTOR, value='a.hintanchor')
+            self.links = [link.get_attribute('href') for link in links]
+
+    def parseTwi(self, txt):
+        assert isinstance(txt, str)
+        return txt.translate(self.twi_translation_table)
 
     def readPage(self, url):
-        pass
+        twi = ''
+        eng = ''
+        title = ''
+        print(f'fetching {url}')
+        self.driver.get(url)
+        print(f'page opened')
+        title = self.driver.find_element(by=By.TAG_NAME, value='b').text
+        div_contains = self.driver.find_elements(
+            by=By.CSS_SELECTOR, value='td>div')
+        twi = div_contains[0].text
+        twi = self.parseTwi(twi)
+        if (len(div_contains) > 1):
+            eng = div_contains[1].text
+        print(title)
+        # print('--------------------------------')
+        print(twi)
+        # print('--------------------------------')
+        print(eng)
+        res = {
+            'twi': twi,
+            'eng': eng,
+            'title': title
+        }
+        self.json_data.append(res)
+        return res
+
+    def readJSON(self):
+        with open(f'../data/hymn_{self.type}', 'r') as f:
+            data = json.load(f)
+        return data
+
+    def writeJSON(self, payload):
+        # Construct the directory and file path
+        directory = '../data/'
+        file_path = f'{directory}hymn_{self.type}.json'
+
+        # Create the directory if it does not exist
+        os.makedirs(directory, exist_ok=True)
+
+        # Write the JSON data to the file
+        with open(file_path, 'w') as f:
+            json.dump(payload, f)
+
+    def reset(self):
+        self.json_data = []
+        self.links = []
+        return
